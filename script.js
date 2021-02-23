9c5d980f0338e6fa1d2cd161884a5421d286e115
Serhii <malgothsm@gmail.com>
2021-02-23 20:27:46 +0200

    Adding cartUpdate

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let removeCardItems = document.getElementsByClassName('btn-danger');
+let removeCartItemsButt = document.getElementsByClassName('btn-danger');
 
-for ( let i =0; i< removeCardItems.length; i++){
-let button = removeCardItems[i];
+for ( let i =0; i< removeCartItemsButt.length; i++){
+let button = removeCartItemsButt[i];
 button.addEventListener('click', (e)=>{
   let buttonClicked = e.target;
   buttonClicked.parentElement.parentElement.remove();
+  updateCardTotal();
 })
+}
+
+let updateCardTotal = () => {
+  let cartItemContainer = document.getElementsByClassName('cart-items')[0];
+  let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+  for ( let i =0; i< cartRows.length; i++){
+    let cartRow = cartRows[i];  
+    let priceElem = cartRow.getElementsByClassName('cart-price')[0];
+  }
 }
