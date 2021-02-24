b8cb74d292f93d38879487a482345e66cb04b2cc
Serhii <malgothsm@gmail.com>
2021-02-24 18:37:30 +0200

    added function for adding items in the cart

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ function ready (){
       let input = quantityInputs[i];
       input.addEventListener('change', quantityChanged);
     }
+    let addToCartButtons = document.getElementsByClassName('shop-item-button');
+    for ( let i = 0; i < addToCartButtons.length; i++) {
+      let button = addToCartButtons[i];
+      button.addEventListener('click', addToCartClicked);
+    }
 }
 
 function removeCardItem(e){
@@ -31,6 +36,42 @@ function quantityChanged(e){
 }
 
 
+function addToCartClicked(e) {
+  let button = e.target;
+  let shotItem = button.parentElement.parentElement;
+  let title = shotItem.getElementsByClassName('shop-item-title')[0].innerText;
+  let price = shotItem.getElementsByClassName('shop-item-price')[0].innerText;
+  let imageSrc = shotItem.getElementsByClassName('shop-item-image')[0].src;
+  console.log(title, price, imageSrc);
+  addItemToCart(title, price, imageSrc);
+}
+
+function addItemToCart(title, price, imageSrc){
+  let cartRow = document.createElement('div');
+  cartRow.classList.add('cart-row');
+  let cartItems = document.getElementsByClassName('cart-items')[0];
+  let cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+  for (let i = 0; i < cartItemNames.length; i++){
+    if (cartItemNames[i].innerText == title) {
+      alert ('This item is already added to the cart!');
+      return;
+    } 
+  }
+  let cartRowContents = `
+  <div class="cart-item cart-column">
+    <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+           <span class="cart-item-title">${title}</span>
+                    </div>
+                    <span class="cart-price cart-column">${price}</span>
+                    <div class="cart-quantity cart-column">
+                        <input class="cart-quantity-input" type="number" value="1">
+                        <button class="btn btn-danger" type="button">REMOVE</button>
+                    </div>
+                </div>`;
+  cartItems.append(cartRow);
+  cartRow.innerHTML = cartRowContents;
+}
+
 function updateCartTotal() {
   let cartItemContainer = document.getElementsByClassName('cart-items')[0];
   let cartRows = cartItemContainer.getElementsByClassName('cart-row')
@@ -43,5 +84,6 @@ function updateCartTotal() {
     let quantity = quantityElem.value;
     total += price*quantity;
   }
+  total = Math.round(total*100)/100;
   document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
 }
